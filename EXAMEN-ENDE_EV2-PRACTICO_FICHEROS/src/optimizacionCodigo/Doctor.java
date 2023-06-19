65692c35eb1b64f371e62a0ecbcdbeab66f7f317
jeremy delgado <jeremynick10@gmail.comgit config --global user.name Jeremygit commit -m Valido>
2023-06-19 10:21:12 +0100

    "VALIDACION INICIAL"

---
+++ EXAMEN-ENDE_EV2-PRACTICO_FICHEROS/src/optimizacionCodigo/Doctor.java
@@ -0,0 +1,22 @@
+package  optimizacionCodigo ;
+
+import java.util.Date;
+
+public class Doctor {
+	
+	private int numeroColegiadoMedicina;
+
+	public void realizarDiagnostico() {
+	}
+
+	public int getNumeroColegiadoMedicina() {
+		return numeroColegiadoMedicina;
+	}
+
+	public void setNumeroColegiadoMedicina(int numeroColegiadoMedicina) {
+		this.numeroColegiadoMedicina = numeroColegiadoMedicina;
+	}
+
+	public void vertificarAlta() {
+	}
+}
---
+++ EXAMEN-ENDE_EV2-PRACTICO_FICHEROS/src/optimizacionCodigo/Enfermero.java
@@ -0,0 +1,25 @@
+package optimizacionCodigo;
+
+import java.util.Date;
+
+public class Enfermero {
+	
+	private int numeroColegiadoEnfermeria;
+
+	public void realizarCuras() {
+	}
+
+
+	public int getNumeroColegiadoEnfermeria() {
+		return numeroColegiadoEnfermeria;
+	}
+
+
+	public void setNumeroColegiadoEnfermeria(int numeroColegiadoEnfermeria) {
+		this.numeroColegiadoEnfermeria = numeroColegiadoEnfermeria;
+	}
+
+
+	public void contactarFamilias() {
+	}
+}
